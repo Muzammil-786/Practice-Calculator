7bb5efbb01959667d87690856cc764b51d466c47
Muzammil-786 <81347490+Muzammil-786@users.noreply.github.com>
2021-06-28 23:56:13 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Calculator(),
+    return MaterialApp(
+      home: Calculator(),
     );
   }
 }
